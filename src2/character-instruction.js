6a7bf1ef833aec81ecf7d72ee0a28669b024ef0b
Abhaya Uprety <auprety@aims1.com>
2018-06-20 16:21:39 -0500

    new source2

---
+++ src2/character-instruction.js
@@ -0,0 +1,46 @@
+
+class CharacterInstruction{
+
+	setInstruction(lineIndex, letterIndex, character){
+
+	}
+
+	enterInstruction(lineIndex, letterIndex){
+
+	}
+
+	backspaceInstruction(lineIndex, letterIndex){
+		if (lineIndex == 0 && letterIndex == 0){
+			return;
+		}
+		else if (letterIndex == 0) {
+
+		}
+		else {
+
+		}
+	}
+
+	characterInstruction(lineIndex, letterIndex, character){
+
+	}
+
+	insertOne(lineIndex, letterIndex, character){
+
+	}
+
+	insertMany(lineIndex, letterIndex, character){
+
+	}
+
+	removeOne(lineIndex, letterIndex, shouldCaretMove){
+
+	}
+
+	removeMany(lineIndex, letterIndex, shouldCaretMove){
+		
+	}
+
+	
+
+}
